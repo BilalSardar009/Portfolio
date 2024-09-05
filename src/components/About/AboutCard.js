96679a23dbc031571a1b841ffc2659f5b232c8dc
Bilal Sardar <bilalsardar009@gmail.com>
2024-09-05 14:06:26 +0100

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,19 +1,15 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import { ImPointRight } from "react-icons/im";
-
-
 function AboutCard() {
   return (
     <Card className="quote-card-view">
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Bilal Sardar</span> from{" "}
-            <span className="purple">Cambridge, United Kingdom.</span>
-            <br />
-            I am currently pursuing my Master's degree in Artificial Intelligence at Anglia Ruskin University, Cambridge.
-            <br />
+            Hi Everyone, I am <span className="purple">Bilal Sardar </span>
+            from <span className="purple"> Cambridge, United Kingdom.</span>
+            <br />I am currently pursuing my Master's degree in Artificial Intelligence at Anglia Ruskin University, Cambridge.
             <br />
             I'm an innovative Machine Learning Engineer with strong AI and deep learning expertise.
             <br />
@@ -38,11 +34,3 @@ function AboutCard() {
         </blockquote>
       </Card.Body>
     </Card>
-  );
-}
-
-export default AboutCard;
-  );
-}
-
-export default AboutCard;
